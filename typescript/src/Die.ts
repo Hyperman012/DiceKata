63da6ddec9aed117cecbfbc30db060bed41278b6
Adam Whaley <abwn83@gmail.com>
2025-07-16 16:14:43 -0400

    adding typescript starter project

---
+++ typescript/src/Die.ts
@@ -0,0 +1,22 @@
+export class Die {
+
+   constructor(front: number, top: number, right: number) {
+   }
+
+   rotateUp(): Die {
+      throw new Error("Not implemented");
+   }
+
+   rotateDown(): Die {
+      throw new Error("Not implemented");
+   }
+
+   rotateLeft(): Die {
+      throw new Error("Not implemented");
+   }
+
+   rotateRight(): Die {
+      throw new Error("Not implemented");
+   }
+}
+
---
+++ typescript/tests/Die.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from "@jest/globals";
+import {Die} from "../src/Die";
+
+describe("Die Tests", () => {
+    it('should ... ', () => {
+    });
+});
